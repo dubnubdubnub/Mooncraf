23bfba948e2d90c003fc644972add73a689405ed
dubnubdubnub <isaacchiu2006@gmail.com>
2021-01-28 18:00:23 -0700

    fewg

--- src/main/java/com/dubnubdubnub/mooncraf/Mooncraf.java
+++ src/main/java/com/dubnubdubnub/mooncraf/Mooncraf.java
@@ -1,19 +1,13 @@
 package com.dubnubdubnub.mooncraf;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -21,8 +15,6 @@ import org.apache.logging.log4j.Logger;
 
 import com.dubnubdubnub.mooncraf.init.ModEntityTypes;
 import com.dubnubdubnub.mooncraf.init.iteminit;
-
-import java.util.stream.Collectors;
 
 @Mod("mooncraf")
 public class Mooncraf
--- src/main/java/com/dubnubdubnub/mooncraf/entities/BasicTurretEntity.java
+++ src/main/java/com/dubnubdubnub/mooncraf/entities/BasicTurretEntity.java
@@ -5,34 +5,18 @@ import com.dubnubdubnub.mooncraf.init.ModEntityTypes;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
-import net.minecraft.entity.monster.AbstractRaiderEntity;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.AbstractArrowEntity;
-import net.minecraft.entity.projectile.PotionEntity;
-import net.minecraft.entity.projectile.ProjectileHelper;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.potion.Effects;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionUtils;
-import net.minecraft.potion.Potions;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public class BasicTurretEntity extends AnimalEntity{ 
@@ -80,27 +64,7 @@ public class BasicTurretEntity extends AnimalEntity{
 	      this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0D);
 	   }
 	
-	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
-	      super.setEquipmentBasedOnDifficulty(difficulty);
-	      this.setItemStackToSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.BOW));
-	   }
-	
-	public void attackEntityWithRangedAttack(LivingEntity target, float distanceFactor) {
-	      ItemStack itemstack = this.findAmmo(this.getHeldItem(ProjectileHelper.getHandWith(this, Items.BOW)));
-	      AbstractArrowEntity abstractarrowentity = this.fireArrow(itemstack, distanceFactor);
-	      if (this.getHeldItemMainhand().getItem() instanceof net.minecraft.item.BowItem)
-	         abstractarrowentity = ((net.minecraft.item.BowItem)this.getHeldItemMainhand().getItem()).customeArrow(abstractarrowentity);
-	      double d0 = target.getPosX() - this.getPosX();
-	      double d1 = target.getPosYHeight(0.3333333333333333D) - abstractarrowentity.getPosY();
-	      double d2 = target.getPosZ() - this.getPosZ();
-	      double d3 = (double)MathHelper.sqrt(d0 * d0 + d2 * d2);
-	      abstractarrowentity.shoot(d0, d1 + d3 * (double)0.2F, d2, 1.6F, (float)(14 - this.world.getDifficulty().getId() * 4));
-	      this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
-	      this.world.addEntity(abstractarrowentity);
-	   }
-	protected AbstractArrowEntity fireArrow(ItemStack arrowStack, float distanceFactor) {
-	      return ProjectileHelper.fireArrow(this, arrowStack, distanceFactor);
-	   }
+
 	
 
 }
